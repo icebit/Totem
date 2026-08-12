b37d96da21ee19624ad0e1110b3d652bf1f96ccc
icebit <samjharrison8@gmail.com>
2026-08-12 11:32:30 -0500

    Revert "feat: C++ conversion"
    This reverts commit 0e556043b313fc2d68a241cb157af3564b17da5d.

--- Source/Totem.Target.cs
+++ Source/Totem.Target.cs
@@ -11,6 +11,5 @@ public class TotemTarget : TargetRules
 		DefaultBuildSettings = BuildSettingsVersion.V5;
 		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_4;
 		ExtraModuleNames.Add("Totem");
-		ExtraModuleNames.Add("TotemGame");
 	}
 }
